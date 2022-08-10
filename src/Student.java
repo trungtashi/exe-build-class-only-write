79ca0affcc8954c9601a343ba0d67b12c44aa9dc
trungtashi99 <trungtashi@gmai.com>
2022-08-10 13:58:46 +0700

    Bài tập xây dựng lớp chỉ ghi trong java

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    private String name = "John";
+    private String classed = "C02";
+
+    Student(){}
+
+    public void setName(String name){
+        this.name = name;
+    }
+    protected void setClassed(String classed){
+        this.classed = classed;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        Student name = new Student();
+        Student classed = new Student();
+
+     name.setName("Thor");
+     classed.setClassed("Avengers");
+
+    }
+}
